997c4c1d9a54fe99fbd46d65f7798346c2d4a83c
“Qurratulainmalik81” <qmalik@keyin.com>
2026-01-15 13:36:12 -0500

    Add Circle and CircleDemo classes with constructors and methods for radius and area

---
+++ Week 2/Circle.java
@@ -0,0 +1,54 @@
+public class Circle{
+    //Instance variables
+    private double radius = 1.0;
+    private String color = "red";
+
+    //1. when your class do not have a constructor, compiler provides a default constructor for you
+    //2. No argument constructor 
+    public Circle()
+    {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+    //3. parameterised construtor
+    public Circle(double r)
+    {
+        this.radius = r;
+    }
+    public Circle(double radius, String color)
+    {
+        this.color = color;
+        this.radius = radius;
+    }
+
+    public double getRadius()
+    {
+        return this.radius;
+    }
+    public String getColor()
+    {
+        return this.color;
+    }
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public String toString()
+    {
+       return("Radius = " + this.radius + " and color =  " + this.color);
+    }
+
+    public double getArea()
+    {
+        return (Math.PI * this.radius*this.radius);
+    }
+
+
+
+
+}
---
+++ Week 2/CircleDemo.java
@@ -0,0 +1,18 @@
+public class CircleDemo{
+    public static void main(String args[]){
+
+        Circle c1 = new Circle();
+        System.out.println(c1);
+        Circle c2 = new Circle(6.5);
+        System.out.println(c2.toString());
+       
+        Circle c3 = new Circle(5.0,"pink");
+        System.out.println(c3);
+
+        System.out.println(c1.getRadius());
+        c1.setRadius(7.0);
+        c1.setColor("yellow");
+         System.out.println(c1);
+          System.out.println(c1.getArea());
+    }
+}
